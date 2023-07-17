bc6e096d2abdc7a219bce76db9e10c5c1f443cc7
squid <datastruct2009@gmail.com>
2023-07-17 16:28:19 +0800

    update new code

--- lecture_01/src/main.rs
+++ lecture_01/src/main.rs
@@ -1,12 +1,14 @@
 mod loop_01;
 mod loop_02;
 
+use loop_02::print_fn;
+
 fn main() {
     println!("submod loop1 print ['a'..'Z']");
     loop_01::print();
     println!();
 
     println!("submod loop2 print ['A'..'z']");
-    loop_02::print_fn::print_pretty();
+    print_fn::print_pretty();
     println!();
 }
